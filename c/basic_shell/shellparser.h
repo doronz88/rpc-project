52f8f7422af4248499677aa93a105132182f2c79
doron zarhi <doron88@gmail.com>
2022-01-26 09:38:23 +0200

    remove compiled files

--- c/basic_shell/shellparser.h
+++
@@ -1,6 +0,0 @@
-#define EOL                              1
-#define AND                              2
-#define LEFT                             3
-#define ARGUMENT                         4
-#define RIGHT                            5
-#define PIPE                             6
