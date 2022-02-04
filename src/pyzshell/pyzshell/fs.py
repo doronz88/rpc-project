bb3069199238b4d7e3f515a6c36ba180a790341a
DoronZ <doron88@gmail.com>
2022-02-04 11:53:57 +0200

    fs: bugfix: handle signed return values

--- src/pyzshell/pyzshell/fs.py
+++ src/pyzshell/pyzshell/fs.py
@@ -9,26 +9,29 @@ class Fs:
         self._client = client
 
     def chmod(self, filename: str, mode: int):
-        """ chmod() filename at remote. read man for more details. """
-        return self._client.symbols.chmod(filename, mode)
+        """ chmod(filename, mode)at remote. read man for more details. """
+        if self._client.symbols.chmod(filename, mode).c_int32 < 0:
+            raise ZShellError(f'failed to chmod: {filename}')
 
     def remove(self, filename: str):
-        """ remove() filename at remote. read man for more details. """
-        return self._client.symbols.remove(filename)
+        """ remove(filename) at remote. read man for more details. """
+        if self._client.symbols.remove(filename).c_int32 < 0:
+            raise ZShellError(f'failed to remove: {filename}')
 
     def mkdir(self, filename: str, mode: int):
-        """ mkdir() filename at remote. read man for more details. """
-        return self._client.symbols.mkdir(filename, mode)
+        """ mkdir(filename, mode) at remote. read man for more details. """
+        if self._client.symbols.mkdir(filename, mode).c_int64 < 0:
+            raise ZShellError(f'failed to mkdir: {filename}')
 
     def write_file(self, filename: str, buf: bytes, mode: int = 0o777) -> None:
         """ write file at remote """
-        fd = self._client.symbols.open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode)
-        if fd == 0xffffffff:
+        fd = self._client.symbols.open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode).c_int32
+        if fd < 0:
             raise ZShellError(f'failed to open: {filename} for writing')
 
         while buf:
-            err = self._client.symbols.write(fd, buf, len(buf))
-            if err == 0xffffffffffffffff:
+            err = self._client.symbols.write(fd, buf, len(buf)).c_int64
+            if err < 0:
                 raise ZShellError(f'write failed for: {filename}')
             buf = buf[err:]
 
@@ -36,14 +39,14 @@ class Fs:
 
     def read_file(self, filename: str) -> bytes:
         """ read file at remote """
-        fd = self._client.symbols.open(filename, O_RDONLY)
-        if fd == 0xffffffff:
+        fd = self._client.symbols.open(filename, O_RDONLY).c_int32
+        if fd < 0:
             raise ZShellError(f'failed to open: {filename} for reading')
 
         buf = b''
         with self._client.safe_malloc(self.CHUNK_SIZE) as chunk:
             while True:
-                err = self._client.symbols.read(fd, chunk, self.CHUNK_SIZE)
+                err = self._client.symbols.read(fd, chunk, self.CHUNK_SIZE).c_int64
                 if err == 0:
                     break
                 elif err < 0:
@@ -54,14 +57,21 @@ class Fs:
 
     def symlink(self, target: str, linkpath: str) -> int:
         """ symlink(target, linkpath) at remote. read man for more details. """
-        err = self._client.symbols.symlink(target, linkpath)
+        err = self._client.symbols.symlink(target, linkpath).c_int64
         if err < 0:
-            raise ZShellError(f'symlink failed for {target} and {linkpath}')
+            raise ZShellError(f'symlink failed to create link: {linkpath}->{target}')
+        return err
+
+    def link(self, target: str, linkpath: str) -> int:
+        """ link(target, linkpath) - hardlink at remote. read man for more details. """
+        err = self._client.symbols.link(target, linkpath).c_int64
+        if err < 0:
+            raise ZShellError(f'link failed to create link: {linkpath}->{target}')
         return err
 
     def pwd(self) -> str:
         """ calls getcwd(buf, size_t) and prints current path.
-            with the special values NULL,NULL the buffer is allocated dynamically """
+            with the special values NULL, 0 the buffer is allocated dynamically """
         chunk = self._client.symbols.getcwd(0, 0)
         if chunk == 0:
             raise ZShellError('pwd failed.')
