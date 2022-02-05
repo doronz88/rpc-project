4c92d8313594c2a797892024a282533ab4f16b49
yotamolenik <39271599+yotamolenik@users.noreply.github.com>
2022-02-05 16:39:56 +0200

    linux_fs: add listdir

--- src/pyzshell/pyzshell/structs/linux.py
+++ src/pyzshell/pyzshell/structs/linux.py
@@ -1,6 +1,7 @@
-from construct import Struct, PaddedString
+from construct import Struct, PaddedString, Int32ul, Int64ul, Int16ul, Int8ul, Bytes, Padding, Computed
 
 _UTSNAME_LENGTH = 65
+_D_NAME_LENGTH = 256
 
 utsname = Struct(
     'sysname' / PaddedString(_UTSNAME_LENGTH, 'utf8'),
@@ -8,4 +9,14 @@ utsname = Struct(
     'release' / PaddedString(_UTSNAME_LENGTH, 'utf8'),
     'version' / PaddedString(_UTSNAME_LENGTH, 'utf8'),
     'machine' / PaddedString(_UTSNAME_LENGTH, 'utf8'),
+)
+
+dirent = Struct(
+    'd_ino' / Int32ul,
+    Padding(4),
+    'd_off' / Int64ul,
+    'd_reclen' / Int16ul,
+    'd_type' / Int8ul,
+    '_d_name_bytes' / Bytes(_D_NAME_LENGTH),
+    'd_name' / Computed(lambda x: x._d_name_bytes.split(b'\x00', 1)[0].decode())
 )
