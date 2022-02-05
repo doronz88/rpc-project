d0b853c0cded047aca5c9c3676a499c6319e2f1d
DoronZ <doron88@gmail.com>
2022-02-06 00:55:09 +0200

    linux_client: add uname

--- src/pyzshell/pyzshell/client/linux_client.py
+++ src/pyzshell/pyzshell/client/linux_client.py
@@ -1,8 +1,18 @@
+from cached_property import cached_property
+
 from pyzshell.client.client import Client
 from pyzshell.linux_fs import LinuxFs
+from pyzshell.structs.linux import utsname
 
 
 class LinuxClient(Client):
-    def __init__(self, sock, sysname: str, hostname: str, port: int = None):
-        super().__init__(sock, sysname, hostname, port)
+    def __init__(self, sock, uname_version: str, hostname: str, port: int = None):
+        super().__init__(sock, uname_version, hostname, port)
         self.fs = LinuxFs(self)
+
+    @cached_property
+    def uname(self):
+        with self.safe_calloc(utsname.sizeof()) as uname:
+            uname.poke(b'\x00' * utsname.sizeof())
+            assert 0 == self.symbols.uname(uname)
+            return utsname.parse_stream(uname)
