ffa1f74c891eec68250f97bd7ba997e60b021908
DoronZ <doron88@gmail.com>
2022-02-17 10:10:15 +0200

    rpcclient: add Allocated

---
+++ src/rpcclient/rpcclient/allocated.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+
+
+class Allocated:
+    """ resource allocated on remote host that needs to be free """
+
+    def __init__(self):
+        self._deallocated = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.deallocate()
+
+    def __del__(self):
+        self.deallocate()
+
+    @abstractmethod
+    def _deallocate(self):
+        pass
+
+    def deallocate(self):
+        if not self._deallocated:
+            self._deallocated = True
+            self._deallocate()
