6f6922713e7f45c1b61b731f581f14f5a8d94bf7
DoronZ <doron88@gmail.com>
2022-02-20 01:04:03 +0200

    darwin: bugfix: seperate roots for ios and macos

--- src/rpcclient/rpcclient/darwin/client.py
+++ src/rpcclient/rpcclient/darwin/client.py
@@ -83,12 +83,7 @@ class DarwinClient(Client):
     @property
     def roots(self) -> typing.List[str]:
         """ get a list of all accessible darwin roots when used for lookup of files/preferences/... """
-        result = ['/']
-        for username in self.fs.scandir('/Users'):
-            if not username.is_dir() or not self.fs.accessible(username.path):
-                continue
-            result.append(username.path)
-        return result
+        return ['/', '/var/root']
 
     def set_airplane_mode(self, mode: bool):
         """ set whether the device should enter airplane mode (turns off baseband, bt, etc...) """
