5b42963120699ac8463500f595423f886129a65c
doron zarhi <doron88@gmail.com>
2022-02-21 14:34:44 +0200

    client: ios: add backlight control

--- src/rpcclient/rpcclient/ios/client.py
+++ src/rpcclient/rpcclient/ios/client.py
@@ -1,9 +1,14 @@
 import typing
 
 from rpcclient.darwin.client import DarwinClient
+from rpcclient.ios.backlight import Backlight
 
 
 class IosClient(DarwinClient):
+    def __init__(self, sock, sysname: str, hostname: str, port: int = None):
+        super().__init__(sock, sysname, hostname, port)
+        self.backlight = Backlight(self)
+
     @property
     def roots(self) -> typing.List[str]:
         """ get a list of all accessible darwin roots when used for lookup of files/preferences/... """
