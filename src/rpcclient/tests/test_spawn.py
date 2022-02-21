e08135df541b51ecdd394f8d8045fbbedc75e85a
doron zarhi <doron88@gmail.com>
2022-02-21 12:58:19 +0200

    tests: test_spawn: reduce stress limit to 100 to avoid kernel kill

--- src/rpcclient/tests/test_spawn.py
+++ src/rpcclient/tests/test_spawn.py
@@ -16,7 +16,7 @@ def test_spawn_sanity(client, argv, expected_stdout, errorcode):
 
 
 def test_spawn_bad_value_stress(client):
-    for i in range(1000):
+    for i in range(100):
         stdout = StringIO()
         assert 256 == client.spawn(['/bin/ls', 'INVALID_PATH'], stdout=stdout, stdin=b'').error
 
