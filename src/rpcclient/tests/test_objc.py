05737fc8adfb7a0d3bb3be39e1c14684412e34b7
matan <matan1008@gmail.com>
2022-02-27 07:58:31 +0200

    Tests: Add Darwin mark.

--- src/rpcclient/tests/test_objc.py
+++
@@ -1,8 +0,0 @@
-def test_objc(client):
-    d = client.symbols.objc_getClass('NSMutableDictionary').objc_call('new').objc_symbol
-
-    # call method
-    d.setObject_forKey_(client.cf('value'), client.cf('key'))
-
-    # call property
-    assert '{\n    key = value;\n}' == d.description.py
