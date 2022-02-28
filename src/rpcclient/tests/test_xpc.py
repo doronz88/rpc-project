01e12d431c2743bea1418a17b464e7ac98eebea7
matan <matan1008@gmail.com>
2022-02-28 16:26:50 +0200

    XPC: Add sending XPC message to Darwin client.

---
+++ src/rpcclient/tests/test_xpc.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.darwin
+def test_from_xpc_object(client):
+    """
+    :param rpcclient.darwin.client.DarwinClient client:
+    """
+    criteria = client.symbols.xpc_dictionary_create(0, 0, 0)
+    client.symbols.xpc_dictionary_set_int64(criteria, 'Delay', 5)
+    client.symbols.xpc_dictionary_set_int64(criteria, 'GracePeriod', 1)
+    client.symbols.xpc_dictionary_set_string(criteria, 'Priority', 'Utility')
+    assert client.xpc.from_xpc_object(criteria) == {'Delay': 5, 'Priority': 'Utility', 'GracePeriod': 1}
