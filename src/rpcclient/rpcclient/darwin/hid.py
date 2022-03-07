f5fb4fc2c0420d547f83b7f3b633db7db7154725
doron zarhi <doron88@gmail.com>
2022-03-07 17:25:03 +0200

    hid: add consts from IOHIDUsageTables and implement

--- src/rpcclient/rpcclient/darwin/hid.py
+++ src/rpcclient/rpcclient/darwin/hid.py
@@ -1,19 +1,11 @@
 import contextlib
-import logging
 import time
-from enum import Enum
 
-from rpcclient.darwin.consts import kCFAllocatorDefault
+from rpcclient.darwin.consts import kCFAllocatorDefault, kHIDUsage_Csmr_Menu, kHIDUsage_Csmr_Power, \
+    kHIDUsage_Csmr_VolumeDecrement, kHIDUsage_Csmr_VolumeIncrement, kHIDPage_Consumer, kHIDUsage_Csmr_Mute, \
+    kHIDUsage_Csmr_ACSearch, kHIDUsage_Csmr_PlayOrPause, kHIDUsage_Csmr_Play, kHIDUsage_Csmr_Pause, \
+    kHIDUsage_Csmr_Rewind, kHIDUsage_Csmr_RandomPlay, kHIDUsage_Csmr_Repeat, kHIDUsage_Csmr_FastForward
 from rpcclient.exceptions import BadReturnValueError
-
-logger = logging.getLogger(__name__)
-
-
-class Keycode(Enum):
-    VOLUME_UP = 0xe9
-    VOLUME_DOWN = 0xea
-    HOME = 0x40
-    POWER = 0x30
 
 
 class Hid:
@@ -24,28 +16,55 @@ class Hid:
         self.send_home_button_press()
         self.send_home_button_press()
 
+    def send_rewind_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Rewind)
+
+    def send_random_play_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_RandomPlay)
+
+    def send_repeat_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Repeat)
+
+    def send_fast_forward_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_FastForward)
+
+    def send_play_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Play)
+
+    def send_pause_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Pause)
+
+    def send_play_pause_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_PlayOrPause)
+
+    def send_search_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_ACSearch)
+
+    def send_mute_button_press(self):
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Mute)
+
     def send_home_button_press(self):
-        self.send_key_press(Keycode.HOME.value)
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Menu)
 
     def send_power_button_press(self, interval: int = 0):
-        self.send_key_press(Keycode.POWER.value, interval=interval)
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_Power, interval=interval)
 
     def send_volume_down_button_press(self, interval: int = 0):
-        self.send_key_press(Keycode.VOLUME_DOWN.value, interval=interval)
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_VolumeDecrement, interval=interval)
 
     def send_volume_up_button_press(self, interval: int = 0):
-        self.send_key_press(Keycode.VOLUME_UP.value, interval=interval)
+        self.send_key_press(kHIDPage_Consumer, kHIDUsage_Csmr_VolumeIncrement, interval=interval)
 
-    def send_key_press(self, key_code: int, interval: int = 0):
-        self.send_keyboard_event(key_code, True)
+    def send_key_press(self, page: int, key_code: int, interval: int = 0):
+        self.send_keyboard_event(page, key_code, True)
         if interval:
             time.sleep(interval)
-        self.send_keyboard_event(key_code, False)
+        self.send_keyboard_event(page, key_code, False)
 
-    def send_keyboard_event(self, key_code: int, down: bool):
+    def send_keyboard_event(self, page: int, key_code: int, down: bool):
         event = self._client.symbols.IOHIDEventCreateKeyboardEvent(kCFAllocatorDefault,
                                                                    self._client.symbols.mach_absolute_time(),
-                                                                   0x0c, key_code, down, 0)
+                                                                   page, key_code, down, 0)
         self.dispatch(event)
 
     @contextlib.contextmanager
