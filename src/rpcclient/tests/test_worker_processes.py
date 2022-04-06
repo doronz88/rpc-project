f2add5784f269650aec1bf6671e768008719538c
assaf0 <asaf.ohayon@outlook.com>
2022-04-06 20:38:17 +0300

    Added test for new worker process design

---
+++ src/rpcclient/tests/test_worker_processes.py
@@ -0,0 +1,11 @@
+from contextlib import closing
+
+from rpcclient.client_factory import create_client
+
+
+def test_new_process_per_client(client):
+    with closing(create_client('127.0.0.1')) as client2:
+        assert client.pid != client2.pid
+        client_process = client.processes.get_by_pid(client.pid)
+        client2_process = client2.processes.get_by_pid(client2.pid)
+        assert client_process.ppid == client2_process.ppid
