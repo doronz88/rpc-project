d1a277f9cae0ca36910f0397811dafe7353529c9
doron zarhi <doron88@gmail.com>
2022-05-26 14:39:23 +0300

    bump version to 3.2.1

--- src/rpcclient/setup.py
+++ src/rpcclient/setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 BASE_DIR = Path(__file__).parent.resolve(strict=True)
-VERSION = '3.2.0'
+VERSION = '3.2.1'
 PACKAGE_NAME = 'rpcclient'
 PACKAGES = [p for p in find_packages() if not p.startswith('tests')]
 
