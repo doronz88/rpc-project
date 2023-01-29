3550bdfb1df92cf81bec6f058d94115f793ec66d
doron zarhi <doron88@gmail.com>
2023-01-29 09:03:34 +0200

    cli: add `-r/--rebind-symbols`

--- src/rpcclient/rpcclient/__main__.py
+++ src/rpcclient/rpcclient/__main__.py
@@ -19,8 +19,12 @@ logging.getLogger('humanfriendly.prompts').disabled = True
 @click.command()
 @click.argument('hostname')
 @click.option('-p', '--port', type=click.INT, default=DEFAULT_PORT)
-def cli(hostname, port):
-    create_tcp_client(hostname, port=port).interactive()
+@click.option('-r', '--rebind-symbols', is_flag=True, help='reload all symbols upon connection')
+def cli(hostname: str, port: int, rebind_symbols: bool):
+    client = create_tcp_client(hostname, port=port)
+    if rebind_symbols:
+        client.rebind_symbols()
+    client.interactive()
 
 
 if __name__ == '__main__':
