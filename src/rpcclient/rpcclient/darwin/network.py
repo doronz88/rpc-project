a1df12e1c36c41556337fdf3cbf32d88847ff15d
doron zarhi <doron88@gmail.com>
2023-02-23 11:57:46 +0200

    network: add `remove_certificate_pinning()`

--- src/rpcclient/rpcclient/darwin/network.py
+++ src/rpcclient/rpcclient/darwin/network.py
@@ -1,3 +1,4 @@
+import sqlite3
 from typing import Mapping
 
 from rpcclient.network import Network
@@ -15,6 +16,21 @@ class DarwinNetwork(Network):
     @property
     def proxy_settings(self) -> Mapping:
         return self._client.symbols.CFNetworkCopySystemProxySettings().py()
+
+    def remove_certificate_pinning(self) -> None:
+        with self._client.fs.remote_file(PINNING_RULED_DB) as local_db_file:
+            # truncate pinning rules
+            conn = sqlite3.connect(local_db_file)
+            cursor = conn.cursor()
+            cursor.execute('DELETE FROM rules')
+            conn.commit()
+            conn.close()
+
+            # push new db
+            self._client.fs.push(local_db_file, PINNING_RULED_DB)
+
+            # restart trustd for changes to take affect
+            self._client.processes.get_by_basename('trustd').kill(SIGKILL)
 
     def flush_dns(self) -> None:
         self._client.processes.get_by_basename('mDNSResponder').kill(SIGKILL)
