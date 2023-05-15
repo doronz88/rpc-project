d4d7e0aa3893b709b4eacd40098b48b36747da96
doron zarhi <doron88@gmail.com>
2023-05-15 11:22:07 +0300

    darwin: add power assertions

---
+++ src/rpcclient/tests/test_power.py
@@ -0,0 +1,40 @@
+import pytest
+
+from rpcclient.darwin.consts import IOPMUserActiveType
+
+pytestmark = pytest.mark.darwin
+
+
+def test_copy_assertions_by_process(client):
+    """
+    :param rpcclient.darwin.client.DarwinClient client:
+    """
+    assertions = client.power.copy_assertions_by_process()
+    assert len(assertions) > 0
+
+
+def test_declare_user_activity(client):
+    """
+    :param rpcclient.darwin.client.DarwinClient client:
+    """
+    name = 'test user activity'
+    with client.power.declare_user_activity(name, IOPMUserActiveType.kIOPMUserActiveLocal):
+        assert client.power.copy_assertions_by_process()[client.pid][0]['AssertName'] == name
+
+
+def test_declare_user_network_activity(client):
+    """
+    :param rpcclient.darwin.client.DarwinClient client:
+    """
+    name = 'test user network activity'
+    with client.power.declare_network_client_activity(name):
+        assert client.power.copy_assertions_by_process()[client.pid][0]['AssertName'] == name
+
+
+def test_create_assertion(client):
+    """
+    :param rpcclient.darwin.client.DarwinClient client:
+    """
+    name = 'test assertion'
+    with client.power.create_assertion(name, 'PreventUserIdleSystemSleep'):
+        assert client.power.copy_assertions_by_process()[client.pid][0]['AssertName'] == name
