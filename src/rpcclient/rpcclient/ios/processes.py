7252e8c987082e112e2721634740e358e82e590a
Netanel Cohen <netanelc305@protonmail.com>
2024-02-26 10:03:20 +0200

    processes: ios: refine `launch`
    Add more options and return `Process` instead of pid

--- src/rpcclient/rpcclient/ios/processes.py
+++ src/rpcclient/rpcclient/ios/processes.py
@@ -1,17 +1,22 @@
+import logging
+from datetime import datetime, timedelta
 from typing import Optional
 
-from rpcclient.darwin.processes import DarwinProcesses
+from rpcclient.darwin.processes import DarwinProcesses, Process
 from rpcclient.exceptions import LaunchError
+from rpcclient.structs.consts import SIGKILL
+
+logger = logging.getLogger(__name__)
 
 
 class IosProcesses(DarwinProcesses):
-
-    def launch(self, bundle_id: str, unlock_device: bool = True, disable_aslr: bool = False,
-               wait_for_debugger: bool = False, stdout: Optional[str] = None,
-               stderr: Optional[str] = None) -> int:
-        """ launch process using BackBoardService
-        https://github.com/swigger/debugserver-ios/blob/master/inc/BackBoardServices.framework/Headers
-        /BackBoardServices.h"""
+    def launch(self, bundle_id: str, kill_existing: bool = True, timeout: float = 1, unlock_device: bool = True,
+               disable_aslr: bool = False, wait_for_debugger: bool = False, stdout: Optional[str] = None,
+               stderr: Optional[str] = None) -> Process:
+        """
+        launch process using BackBoardService
+        https://github.com/swigger/debugserver-ios/blob/master/inc/BackBoardServices.framework/Headers/BackBoardServices.h
+        """
         debug_options = {}
         options = {}
         sym = self._client.symbols
@@ -21,17 +26,24 @@ class IosProcesses(DarwinProcesses):
             debug_options[sym.BKSDebugOptionKeyStandardOutPath[0].py()] = stdout
         if stderr is not None:
             debug_options[sym.BKSDebugOptionKeyStandardErrorPath[0].py()] = stderr
-
         options[sym.BKSOpenApplicationOptionKeyUnlockDevice[0].py()] = unlock_device
         options[sym.BKSOpenApplicationOptionKeyDebuggingOptions[0].py()] = debug_options
 
-        bkssystem_service = self._client.objc_get_class('BKSSystemService').new().objc_symbol
-        bkssystem_service.openApplication_options_clientPort_withResult_(self._client.cf(bundle_id),
-                                                                         self._client.cf(options),
-                                                                         bkssystem_service.createClientPort(),
-                                                                         self._client.get_dummy_block())
+        bkssystem_service = self._client.symbols.objc_getClass('BKSSystemService').objc_call('new')
+        pid = bkssystem_service.objc_call('pidForApplication:', self._client.cf(bundle_id)).c_int32
+        if pid != -1 and kill_existing:
+            logger.info(f'Kill existing process {pid}')
+            self.kill(pid, SIGKILL)
 
-        pid = bkssystem_service.pidForApplication_(self._client.cf(bundle_id)).c_int32
+        bkssystem_service.objc_call(
+            'openApplication:options:clientPort:withResult:', self._client.cf(bundle_id), self._client.cf(options),
+            bkssystem_service.objc_call('createClientPort'), self._client.get_dummy_block())
+
+        start_time = datetime.now()
+        timeout = timedelta(seconds=timeout)
+        while datetime.now() - start_time < timeout:
+            pid = bkssystem_service.objc_call('pidForApplication:', self._client.cf(bundle_id)).c_int32
+
         if pid == -1:
             raise LaunchError()
-        return pid
+        return self.get_by_pid(pid)
