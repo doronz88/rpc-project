a62a07b3ee2fd67dee74ad00d2ecafdf51429acc
zqxwce <yyyyyy6yyyyyy6@gmail.com>
2025-04-09 12:17:37 +0300

    capture_fd: implement CaptureFD
    tests: add capture_fd tests

---
+++ src/rpcclient/tests/test_capture_fd.py
@@ -0,0 +1,38 @@
+import pytest
+
+from rpcclient.capture_fd import CaptureFD
+
+STDOUT_FD = 1
+STDERR_FD = 2
+TEST_DATA: bytes = b'testtesttest'
+
+
+@pytest.mark.parametrize('fd', [
+    STDOUT_FD,
+    STDERR_FD
+])
+def test_capture_fd(client, fd: int) -> None:
+    """
+    :param rpcclient.client.Client client:
+    :param int fd:
+    """
+    with CaptureFD(client, fd) as cap:
+        if -1 == client.symbols.write(fd, TEST_DATA, len(TEST_DATA)):
+            client.raise_errno_exception('write to fd failed')
+        data = cap.read()
+    assert TEST_DATA == data
+
+
+@pytest.mark.parametrize('fd', [
+    STDOUT_FD,
+    STDERR_FD
+])
+def test_fd_cleanup(client, fd: int) -> None:
+    """
+    :param rpcclient.client.Client client:
+    :param int fd:
+    """
+    fd_count = len(client.processes.get_self().fds)
+    with CaptureFD(client, fd):
+        pass
+    assert fd_count == len(client.processes.get_self().fds)
